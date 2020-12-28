c0264e704499b899ae665ebcc122d264ae79a688
benku321 <benkuamv@gmail.com>
2020-12-28 02:33:44 -0500

    add fitures

--- anagramapp/views.py
+++ anagramapp/views.py
@@ -4,19 +4,25 @@ from itertools import permutations
 
 
 def generator_anagram(word):
-    '''stands for yielding 1 words at a time'''
+    """stands for yielding 1 words at a time"""
     for i in permutations(word, len(word)):
         yield ''.join(i)
 
 
 def anagram_form(request):
     search_query = request.GET.get('search')
+    number_of_outputs = request.GET.get("limitation")
     anagram_text = 'Here you will see your anagram'
     list_of_words = []
 
+    if number_of_outputs:
+        number_of_outputs = int(number_of_outputs)
+    else:
+        number_of_outputs = 10
+
     if search_query:
         words = generator_anagram(search_query)
-        for i in range(10):
+        for i in range(number_of_outputs):
             try:
                 list_of_words.append(''.join(next(words)))
             except StopIteration:
